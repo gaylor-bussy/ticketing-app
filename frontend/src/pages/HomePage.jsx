dcd7ab84c06447f39f6da8572660042da2428c2f
Manuel MATTANA <manuel.mattana@hotmail.fr>
2026-06-23 17:27:36 +0200

    17h27

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,48 +1,60 @@
 import { useState } from "react";
+import { Next } from "react-bootstrap/esm/PageItem";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
-  const [nextPage,setNextPage] = useState(false);
+  const [nextPage, setNextPage] = useState(false);
 
-  function handleNext(){
+  function handleNext() {
     setNextPage(true);
   }
   return (
-    <section className="flex justify-center h-full items-center" >
-      <form action="" className="flex flex-col w-xl" method="post">
-        <div className="my-4">
-          <label className="text-white text-2xl" htmlFor="">Numéro AFPA :</label>
-          <input
-            type="text"
-            placeholder="Saisissez votre numéro AFPA"
-            className="input input-success w-full placeholder:text-xl h-12"
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-white text-2xl" htmlFor="">Nom :</label>
-          <input
-            type="text"
-            placeholder="Saisissez votre Nom"
-            className="input input-success w-full placeholder:text-xl h-12"
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-white text-2xl" htmlFor="">Prénom :</label>
-          <input
-            type="text"
-            placeholder="Saisissez votre Prénom"
-            className="input input-success w-full placeholder:text-xl h-12"
-          />
-        </div>
-        <Link to="/next">
-          <button onClick={handleNext}
-            className="btn btn-success h-14 w-full text-2xl my-4 text-black"
-            type="submit"
-          >
-            Suivant
-          </button>
-        </Link>
-      </form>
+    <section className="flex justify-center h-full items-center">
+      {nextPage ? (
+        div
+      ) : (
+        <form action="" className="flex flex-col w-xl" method="post">
+          <div className="my-4">
+            <label className="text-white text-2xl" htmlFor="">
+              Numéro AFPA :
+            </label>
+            <input
+              type="text"
+              placeholder="Saisissez votre numéro AFPA"
+              className="input input-success w-full placeholder:text-xl h-12"
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-white text-2xl" htmlFor="">
+              Nom :
+            </label>
+            <input
+              type="text"
+              placeholder="Saisissez votre Nom"
+              className="input input-success w-full placeholder:text-xl h-12"
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-white text-2xl" htmlFor="">
+              Prénom :
+            </label>
+            <input
+              type="text"
+              placeholder="Saisissez votre Prénom"
+              className="input input-success w-full placeholder:text-xl h-12"
+            />
+          </div>
+          <Link to="/next">
+            <button
+              onClick={handleNext}
+              className="btn btn-success h-14 w-full text-2xl my-4 text-black"
+              type="submit"
+            >
+              Suivant
+            </button>
+          </Link>
+        </form>
+      )}
     </section>
   );
 }

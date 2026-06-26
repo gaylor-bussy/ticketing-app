5d4b2afeb3f2ba51793e293e2485ea7931ab9425
rivs-k <rivsdev@outlook.fr>
2026-06-26 09:14:25 +0200

    0914

--- frontend/src/pages/Dashboard_Manageur.jsx
+++ frontend/src/pages/Dashboard_Manageur.jsx
@@ -3,7 +3,7 @@ import Table_Demandes from "../components/Table_Demandes";
 import Search_Bar from "../components/Search_Bar";
 import Modal_Add_Demande from "../components/Modal_Add_Demande";
 
-export default function Dashboard_Manager({userr}) {
+export default function Dashboard_Manager({ userr }) {
     const [demandes, setDemandes] = useState([]);
     const [search, setSearch] = useState("");
     const [showModal, setShowModal] = useState(false);
@@ -80,7 +80,7 @@ export default function Dashboard_Manager({userr}) {
             },
         );
 
-        
+
         const data = await response.json();
         console.log(data);
 
@@ -194,70 +194,73 @@ export default function Dashboard_Manager({userr}) {
         }
     };
 
-  return (
-    <section className="flex flex-col p-6">
-      <div className="flex justify-between">
-        <p className="text-white mb-6">Bonjour {userr.nom}</p>
-        <p className="text-white">Role du compte : {userr.nom_role}</p>
-      </div>
-      <section className="flex justify-between items-center mb-4">
-        <Search_Bar search={search} setSearch={setSearch} />
-
-        <button className="btn btn-success" onClick={() => setShowModal(true)}>
-          Ajouter une demande
-        </button>
-      </section>
-
-      <Table_Demandes
-        demandes={demandesFiltrees}
-        isManageur={true}
-        ouvrirModalPriorite={ouvrirModalPriorite}
-        idUser={user?.id_user}
-      />
-
-      <Modal_Add_Demande
-        showModal={showModal}
-        setShowModal={setShowModal}
-        description={description}
-        setDescription={setDescription}
-        priorite={priorite}
-        setPriorite={setPriorite}
-        ajouterDemande={ajouterDemande}
-      />
-
-      {showPrioriteModal && (
-        <dialog className="modal modal-open">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg mb-4">Modifier la priorité</h3>
-
-            <select
-              className="select select-bordered w-full mb-4"
-              value={nouvellePriorite}
-              onChange={(e) => setNouvellePriorite(e.target.value)}
-            >
-              <option value="1">Urgent</option>
-              <option value="2">Peut attendre</option>
-              <option value="3">Pressant</option>
-            </select>
-
-            <div className="modal-action">
-              <button
-                className="btn btn-error"
-                onClick={() => setShowPrioriteModal(false)}
-              >
-                Annuler
-              </button>
-
-              <button
-                className="btn btn-success"
-                onClick={validerChangementPriorite}
-              >
-                Valider
-              </button>
+    return (
+        <section className="flex flex-col p-6">
+            <div className="flex justify-between">
+                <p className="text-white mb-6">Bonjour {userr.nom}</p>
+                <p className="text-white">Role du compte : {userr.nom_role}</p>
             </div>
-          </div>
-        </dialog>
-      )}
-    </section>
-  );
+            <section className="flex justify-between items-center mb-4">
+                <Search_Bar search={search} setSearch={setSearch} />
+
+                <button className="btn btn-success" onClick={() => setShowModal(true)}>
+                    Ajouter une demande
+                </button>
+            </section>
+
+            <Table_Demandes
+                demandes={demandesFiltrees}
+                isManageur={true}
+                positionnerDemande={positionnerDemande}
+                ouvrirModalPriorite={ouvrirModalPriorite}
+                approuverPositionnement={approuverPositionnement}
+                refuserPositionnement={refuserPositionnement}
+                idUser={user?.id_user}
+            />
+
+            <Modal_Add_Demande
+                showModal={showModal}
+                setShowModal={setShowModal}
+                description={description}
+                setDescription={setDescription}
+                priorite={priorite}
+                setPriorite={setPriorite}
+                ajouterDemande={ajouterDemande}
+            />
+
+            {showPrioriteModal && (
+                <dialog className="modal modal-open">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg mb-4">Modifier la priorité</h3>
+
+                        <select
+                            className="select select-bordered w-full mb-4"
+                            value={nouvellePriorite}
+                            onChange={(e) => setNouvellePriorite(e.target.value)}
+                        >
+                            <option value="1">Urgent</option>
+                            <option value="2">Peut attendre</option>
+                            <option value="3">Pressant</option>
+                        </select>
+
+                        <div className="modal-action">
+                            <button
+                                className="btn btn-error"
+                                onClick={() => setShowPrioriteModal(false)}
+                            >
+                                Annuler
+                            </button>
+
+                            <button
+                                className="btn btn-success"
+                                onClick={validerChangementPriorite}
+                            >
+                                Valider
+                            </button>
+                        </div>
+                    </div>
+                </dialog>
+            )}
+        </section>
+    );
 }

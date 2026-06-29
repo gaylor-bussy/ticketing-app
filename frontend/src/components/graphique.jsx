74f561509be3d93a5a326406d0938ac883f7182e
gaylor-bussy <gaylor.bussy@gmail.com>
2026-06-29 09:51:29 +0200

    graphique

--- frontend/src/components/graphique.jsx
+++ frontend/src/components/graphique.jsx
@@ -1,41 +1,78 @@
 import Box from '@mui/material/Box';
 import { LineChart } from '@mui/x-charts/LineChart';
+import { useState, useEffect } from "react";
 
 
 
 export default function Graphique() {
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const xLabels = [
-  'Janvier',
-  'Février',
-  'Mars',
-  'Avril',
-  'Mai',
-  'Juin',
-  'Juillet',
-  'Août',
-  'Septembre',
-  'Octobre',
-  'Novembre',
-  'Decembre',
+    const [demandes, setDemandes] = useState([]);
 
+
+    useEffect(() => {
+    const token = localStorage.getItem("token");
+    fetch("http://localhost:3000/dashboard/manageur/graphique", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+   .then((data) => {
+    console.log(data);
+    console.log(Array.isArray(data));
+    setDemandes(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+    console.log(demandes)
+  const mois = [
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
 ];
+  const margin = { right: 24 };
 
-export default function BiaxialLineChart() {
+const xLabels = demandes.map((d) => mois[d.mois - 1]);
+const crees = demandes.map((d) => d.crees);
+const realisees = demandes.map((d) => d.realisees);
+ 
+
+
+
+   
+
   return (
-    <Box sx={{ width: '100%', height: 300 }}>
+
+
+       <Box sx={{ width: '100%', height: 300 }}>
       <LineChart
-        series={[
-          { data: pData, label: 'pv', yAxisId: 'leftAxisId' },
-          { data: uData, label: 'uv', yAxisId: 'rightAxisId' },
-        ]}
-        xAxis={[{ scaleType: 'point', data: xLabels, height: 28 }]}
-        yAxis={[
-          { id: 'leftAxisId', width: 50 },
-          { id: 'rightAxisId', position: 'right' },
-        ]}
-      />
+  series={[
+    {
+      data: crees,
+      label: "Demandes crées",
+    },
+    {
+      data: realisees,
+      label: "Demandes réalisées",
+    },
+  ]}
+  xAxis={[
+    {
+      scaleType: "point",
+      data: xLabels,
+    },
+  ]}
+  yAxis={[{ width: 50 }]}
+/>
     </Box>
   );
 }
@@ -57,4 +94,3 @@ export default function BiaxialLineChart() {
 
 
 
-}

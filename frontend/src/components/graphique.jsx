c0876fa770e71e42643e6f7912c48fe3ce4ef657
gaylor-bussy <gaylor.bussy@gmail.com>
2026-06-29 13:43:03 +0200

    export excel

--- frontend/src/components/graphique.jsx
+++ frontend/src/components/graphique.jsx
@@ -1,14 +1,14 @@
 import Box from '@mui/material/Box';
 import { LineChart } from '@mui/x-charts/LineChart';
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
+export default function Graphique({userr}) {
+  const [demandes, setDemandes] = useState([]);
 
-export default function Graphique() {
-    const [demandes, setDemandes] = useState([]);
 
-
-    useEffect(() => {
+  useEffect(() => {
     const token = localStorage.getItem("token");
     fetch("http://localhost:3000/dashboard/manageur/graphique", {
       method: "GET",
@@ -17,42 +17,55 @@ export default function Graphique() {
       },
     })
       .then((response) => response.json())
-   .then((data) => {
-    console.log(data);
-    console.log(Array.isArray(data));
-    setDemandes(data);
+      .then((data) => {
+        console.log(data);
+        console.log(Array.isArray(data));
+        setDemandes(data);
       })
       .catch((error) => console.error(error));
   }, []);
-    console.log(demandes)
+  console.log(demandes)
   const mois = [
-  "Janvier",
-  "Février",
-  "Mars",
-  "Avril",
-  "Mai",
-  "Juin",
-  "Juillet",
-  "Août",
-  "Septembre",
-  "Octobre",
-  "Novembre",
-  "Décembre",
-];
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre",
+  ];
   const margin = { right: 24 };
 
-const xLabels = demandes.map((d) => mois[d.mois - 1]);
-const crees = demandes.map((d) => d.crees);
-const realisees = demandes.map((d) => d.realisees);
- 
+  const xLabels = demandes.map((d) => mois[d.mois - 1]);
+  const crees = demandes.map((d) => d.crees);
+  const realisees = demandes.map((d) => d.realisees);
 
 
 
-   
+
+
 
   return (
-
-
+        <>
+             <section className="flex flex-col p-6">
+            <div className="flex justify-between">
+                <p className="text-white mb-6">Bonjour {userr.nom}</p>
+                 
+                <p className="text-white">Role du compte : {userr.nom_role}</p>
+               
+             </div>
+              <Link  to={"/dashboard/manageur"}>
+                <button className="btn btn-success " >
+                Retour
+                </button>
+                </Link>
+             </section>
+        <div>
        <Box sx={{ width: '100%', height: 300 }}>
       <LineChart
   series={[
@@ -74,9 +87,10 @@ const realisees = demandes.map((d) => d.realisees);
   yAxis={[{ width: 50 }]}
 />
     </Box>
+    </div>
+    </>
   );
 }
-
 
 
 

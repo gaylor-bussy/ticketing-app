5e09c02ca2d7422e27edfd2e6f64e55d7396e1af
Manuel MATTANA <manuel.mattana@hotmail.fr>
2026-07-01 10:28:43 +0200

    review menu

--- frontend/src/pages/Result_Demand.jsx
+++ frontend/src/pages/Result_Demand.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 
-export default function Result_Demand({idDemandeInvitee}) {
+export default function Result_Demand({idDemandeInvitee , setColorChange}) {
   const idDemandeInvite = localStorage.getItem("id_demande_invite");
+  function noColorChangee() {
+    setColorChange(true);
+  }
+  
   return (
     <section className="mx-auto flex min-h-full max-w-4xl flex-col justify-center gap-4 px-4 py-8 sm:px-6 lg:py-10">
       <h1 className="text-center text-2xl text-white sm:text-4xl lg:text-5xl">Votre Numéro de demande est : </h1>
@@ -10,7 +14,7 @@ export default function Result_Demand({idDemandeInvitee}) {
         Merci de bien vouloir garder ce numéro, il vous servira pour suivre
         votre demande.
       </p>
-      <p className="text-center text-base text-white sm:text-2xl lg:text-3xl"><Link to={"/demand-invite-show2"} className="text-blue-500">Cliquez-ici</Link> pour voir votre demande en cours</p>
+      <p className="text-center text-base text-white sm:text-2xl lg:text-3xl"><Link to={"/demand-invite-show2"} onClick={noColorChangee} className="text-blue-500">Cliquez-ici</Link> pour voir votre demande en cours</p>
     </section>
   );
 }
